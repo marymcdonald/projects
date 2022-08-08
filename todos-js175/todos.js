5c9a917a5448b9eb9c9fb3b5996cf29c29a3ded9
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2022-08-08 12:03:37 -0400

    Added flash messages + installed express-validator

--- todos-js175/todos.js
+++ todos-js175/todos.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const { body, validationResult } = require("express-validator");
 
 const app = express();
 const host = "localhost";
@@ -16,6 +19,20 @@ app.set("view engine", "pug");
 app.use(morgan("common"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  name: "launch-school-todos-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+}));
+app.use(flash());
+
+//Extract session info
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
 const compareByTitle = lists => {
   return lists.slice().sort((todoListA, todoListB) => {
@@ -62,21 +79,25 @@ app.get("/lists", (req, res) => {
 app.post("/lists", (req, res) => {
   let title = req.body.todoListTitle.trim();
   if (title.length === 0) {
+    req.flash("error", "A title was not provided.");
     res.render("new-list", {
-      errorMessage: "A title was not provided",
+      flash: req.flash(),
     });
   } else if (title.length > 100) {
+    req.flash("error", "List title must be between 1 and 100 characters.");
     res.render("new-list", {
-      errorMessage: "List title must be between 1 and 100 characters.",
+      flash: req.flash(),
       todoListTitle: title,
     });
   } else if (todoLists.some(list => list.title === title)) {
+    req.flash("error", "List title must be unique.");
     res.render("new-list", {
-      errorMessage: "List title must be unique.",
+      flash: req.flash(),
       todoListTitle: title,
     });
   } else {
     todoLists.push(new TodoList(title));
+    req.flash("success", "The todo list has been created.");
     res.redirect("/lists");
   }
 });
